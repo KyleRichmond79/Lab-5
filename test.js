a6b0c7567e82ca476c606c6e19712742125435e7
Kyle Richmond <kylerichmond79@gmail.com>
2017-06-09 15:43:01 -0700

    completed prob2

--- test.js
+++ test.js
@@ -7,7 +7,7 @@ function testSum() { //eslint-disable-line
     console.log('%c TEST FOR sum() FAILS', 'color: red');
   }
 }
-/*
+
 function testMultiply() { //eslint-disable-line
   if (multiply(5,9)[1] === 'The product of 5 and 9 is 45.') {
     console.log('%c TEST FOR multiply() PASSES', 'color: green');
@@ -15,7 +15,7 @@ function testMultiply() { //eslint-disable-line
     console.log('%c TEST FOR multiply() FAILS', 'color: red');
   }
 }
-
+/*
 function testSumAndMultiply() { //eslint-disable-line
   if (sumAndMultiply(4, 7, 5)[2] === '4 and 7 and 5 sum to 16.' && sumAndMultiply(4, 7, 5)[3] === 'The product of 4 and 7 and 5 is 140.') {
     console.log('%c TEST FOR sumAndMultiply() PASSES', 'color: green');
@@ -39,4 +39,5 @@ function testMultiplyArray() { //eslint-disable-line
     console.log('%c TEST FOR multiplyArray() FAILS', 'color: red');
   }
 }
+
 */
